0de5db69a5d5cdef6021473b78cb506704abe056
ealmih <wxnik1rmuwfa@mail.ru>
2024-04-04 10:43:11 +0300

    добавил описние

--- MainApp/models.py
+++ MainApp/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    count = models.PositiveIntegerField() 
+    count = models.PositiveIntegerField()
+    description = models.TextField(max_length=1000, default='Базовое описание')
