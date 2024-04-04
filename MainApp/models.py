ae942ac177b403cba6d4579bd2ed34e0c16a33c8
ealmih <wxnik1rmuwfa@mail.ru>
2024-04-04 11:18:59 +0300

    добавил цвета

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+class Color(models.Model):
+    name = models.CharField(max_length=32)
+
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     description = models.TextField(max_length=1000, default='Базовое описание')
+    colors = models.ManyToManyField(to=Color)
+
